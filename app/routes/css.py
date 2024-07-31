e26ca79d8207bea498eee6cddd4dd0a71da44cac
yoon741 <mono74140@gmail.com>
2024-07-31 12:19:43 +0900

    css3 - 선택자, CSS리셋

--- app/routes/css.py
+++ app/routes/css.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Request
 from starlette.templating import Jinja2Templates
+
 
 # 라우터 생성
 css_router = APIRouter()
@@ -7,3 +8,17 @@ css_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
 # 라우트 설정
+@css_router.get('/')
+async def index(req: Request):
+    return templates.TemplateResponse('css/index.html', {'request': req})
+@css_router.get('/selector')
+async def selector(req: Request):
+    return templates.TemplateResponse('css/01selector.html', {'request': req})
+
+@css_router.get('/reset')
+async def reset(req: Request):
+    return templates.TemplateResponse('css/02reset.html', {'request': req})
+
+@css_router.get('/text')
+async def text(req: Request):
+    return templates.TemplateResponse('css/03text.html', {'request': req})
