8a25c706f0c55244caaeae43c14794b32f8540ae
yoon741 <mono74140@gmail.com>
2024-08-05 17:19:01 +0900

    자바 스크립트 개요, 자료형

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.templating import Jinja2Templates
 
 # 라우터 생성
 jscript_router = APIRouter()
 # 템플릿 지정
 templates = Jinja2Templates(directory='views/templates')
-
+@jscript_router.get('/')
+async def index(req: Request):
+    return templates.TemplateResponse('js/index.html', {'request': req})
 # 라우트 설정
+@jscript_router.get('/hello')
+async def hello(req: Request):
+    return templates.TemplateResponse('js/01hello.html', {'request': req})
+@jscript_router.get('/type')
+async def type(req: Request):
+    return templates.TemplateResponse('js/02type.html', {'request': req})
---
+++ views/static/js/hello.js
@@ -0,0 +1,3 @@
+function sayhello2() {
+    alert('Hello, again!!')
+}
